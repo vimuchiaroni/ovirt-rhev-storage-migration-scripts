7695521f51aaf427a4b28060c20b6bfa643d5fef
vimuchiaroni <47416373+vimuchiaroni@users.noreply.github.com>
2020-06-01 11:10:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ovirtsla',
+    name='ovirt-rhev-storage-migration',
     version='0.0.1',
     description='a pip-installable package of Api sources',
     license='SLA',
     install_requires=['ovirt-engine-sdk-python'],
-    packages=['ovirtsla'],
+    packages=['ovirt-rhev-storage-migratio'],
     author='Victor Muchiaroni',
     author_email='victormuchiaroni@hotmail.com,
     keywords=['apikey'],
-    url='http://github.com/vimuchiaroni/ovirt.git'
+    url='http://github.com/vimuchiaroni/ovirt-rhev-storage-migration-scripts.git'
 )
